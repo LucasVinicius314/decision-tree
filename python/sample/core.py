edf27dbb6fec81a6249350b6f72465351fbf107e
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-09-02 10:00:54 -0300

    v2

--- python/sample/core.py
+++ python/sample/core.py
@@ -1,17 +1,18 @@
 import graphviz
 
-from pandas import read_csv, get_dummies
-from sklearn.metrics import classification_report, confusion_matrix
+from pandas import read_csv, get_dummies, DataFrame
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
-def numetric_transform(data):
-  for column in data:
-    data[column] = data[column].astype(str)
-    data[column] = LabelEncoder().fit_transform(data[column])
-  
-  return data.astype(float)
+labelEncoder = LabelEncoder()
+
+def numeric_transform(data: DataFrame, columns: list[str]) -> DataFrame:
+  for column in columns:
+    data[column] = labelEncoder.fit_transform(data[column].astype(str))
+
+  return data
 
 # Data input.
 data = read_csv('input/main.csv', sep=',').drop('Exemplo', axis=1)
@@ -20,28 +21,60 @@ data = read_csv('input/main.csv', sep=',').drop('Exemplo', axis=1)
 print(data.to_string)
 
 # Numeric transformation.
-one_hot_data = numetric_transform(data).drop('conc', axis=1)
+numeric_data = numeric_transform(
+  data, 
+  [
+    'Alternativo', 
+    'Bar',
+    'Sex/Sab',
+    'fome',
+    'Chuva',
+    'Res',
+  ]
+).drop('conc', axis=1)
+
+treated_data = get_dummies(
+  numeric_data, 
+  [
+    'Cliente',
+    'Preço',
+    'Tipo',
+    'Tempo',
+  ]
+)
 
 # Map data into x and y.
-x, y = one_hot_data, data['conc']
+x, y = treated_data, data['conc']
 
 # Training the main classifier.
 classifier = DecisionTreeClassifier(criterion='entropy')
 classifier.fit(x, y)
 
-# Exporting the main classifier's decision graph.
-dot_data = export_graphviz(
-  classifier,
-  out_file=None,
-  feature_names=list(one_hot_data.columns.values), class_names=y.unique()
-) 
-graph = graphviz.Source(dot_data)
-graph.render('output/main')
+class_names = [
+  'Sim',
+  'Não',
+]
+
+# Exporting the main classifier's decision graph. 
+graphviz.Source(
+  export_graphviz(
+    classifier,
+    out_file=None,
+    feature_names=list(treated_data.columns.values), 
+  )
+).render('output/main')
 
 ###############
 
 # Splitting data for partial training.
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+x_train, x_test, y_train, y_test = train_test_split(
+  x,
+  y,
+  test_size=0.20,
+  random_state=0
+)
+# Printing the treated data that will be worked on.
+print(x_train.to_string())
 
 # Training the test classifier.
 classifier = DecisionTreeClassifier(criterion='entropy')
@@ -51,14 +84,39 @@ classifier.fit(x_train, y_train)
 y_pred = classifier.predict(x_test)
 
 # Printing metrics.
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+conf_matrix = confusion_matrix(y_test, y_pred)
+classif_report = classification_report(y_test, y_pred)
+acc_score = accuracy_score(y_test, y_pred)
 
-# Exporting the test classifier's decision graph.
-dot_data = export_graphviz(
-  classifier,
-  out_file=None,
-  feature_names=list(one_hot_data.columns.values), class_names=y.unique()
-) 
-graph = graphviz.Source(dot_data)
-graph.render('output/main-test')
+true_negatives, false_positives, false_negatives, true_positives = conf_matrix.ravel()
+
+a = true_negatives + false_positives
+b = false_negatives + true_positives
+
+true_negative_rate = str(true_negatives / a)
+false_positive_rate = str(false_positives / a)
+false_negative_rate = str(false_negatives / b)
+true_positive_rate = str(true_positives / b)
+
+print(
+  '\nTrue positive rate: ' + true_positive_rate +
+  '\nTrue negative rate: ' + true_negative_rate +
+  '\nFalse positive rate: ' + false_positive_rate +
+  '\nFalse negative rate: ' + false_negative_rate +
+  '\nAccuracy score: ' + str(acc_score)
+)
+
+print('\nconfusion matrix')
+print(conf_matrix)
+
+print('\nclassification report')
+print(classif_report)
+
+# Exporting the test classifier's decision graph. 
+graphviz.Source(
+  export_graphviz(
+    classifier,
+    out_file=None,
+    feature_names=list(treated_data.columns.values), 
+  )
+).render('output/main-test')
